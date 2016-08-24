e9af72c2712403ad96e4dc512b45910dc8f93b41
kingsofcoding <simplecooldude1@gmail.com>
2016-08-24 04:09:47 +0000

    regex for bold

--- src/marka.js
+++ src/marka.js
@@ -1,7 +1,8 @@
 (function() {
     
   var marka = function(md) {
-    return md.replace(/\[(.*?)\]\((.*?)\)/ig, '<a href=\'$2\'>$1</a>');
+    return md.replace(/\[(.*?)\]\((.*?)\)/ig, '<a href=\'$2\'>$1</a>') // links
+             .replace(/\*\*(.*?)\*\*/ig)
   };
   
   
