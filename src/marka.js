22ba50ef1524386de260c2ebbfa4673d50caaee7
kingsofcoding <simplecooldude1@gmail.com>
2016-08-24 04:04:22 +0000

    basic

--- src/marka.js
+++ src/marka.js
@@ -1,16 +1,13 @@
-(function(window) {
+(function() {
     
   var marka = function(md) {
-    
+    return md.replace(/\[(.*?)\]\((.*?)\)/ig, '<a href=\'$2\'>$1</a>');
   };
-
-
+  
   
   if (typeof module !== 'undefined' && typeof exports === 'object') {
     module.exports = marka;
   } else {
-      window.marka = function(md) {
-        return new marka(md);
-      };
+      window.marka = marka;
   }
-})(window);
+})();
