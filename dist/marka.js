a7282eaf2bfe483ddb6321f0079e881d4f2a42bb
Kabir Shah <simplecooldude1@gmail.com>
2016-08-24 22:05:01 +0000

    gulp size and build

---
+++ dist/marka.js
@@ -0,0 +1,30 @@
+/*
+* Marka 0.0.0
+* Copyright (c) 2016, Kabir Shah
+* http://github.com/KingPixil/marka/
+* Free to use under the MIT license.
+* http://kingpixil.github.io/license
+*/
+(function() {
+    
+  var marka = function(md) {
+    return md.replace(/\[(.*?)\]\((.*?)\)/ig, '<a href=\'$2\'>$1</a>') // links
+             .replace(/\*\*(.*?)\*\*/ig, '<strong>$1</strong>') // bold
+             .replace(/__(.*?)__/ig, "<strong>$1</strong>") //bold
+             .replace(/\*(.*?)\*/ig, '<em>$1</em>') // italics
+             .replace(/_(.*?)_/ig, "<em>$1</em>") // italics
+             .replace(/^\s*#\s+(.*?$)/ig, "<h1>$1</h1>") // h1
+             .replace(/^\s*##\s+(.*?$)/ig, "<h2>$1</h2>") // h2
+             .replace(/^\s*###\s+(.*?$)/ig, "<h3>$1</h3>") // h3
+             .replace(/^\s*####\s+(.*?$)/ig, "<h4>$1</h4>") // h4
+             .replace(/^\s*#####\s+(.*?$)/ig, "<h5>$1</h5>") // h5
+             .replace(/^\s*######\s+(.*?$)/ig, "<h6>$1</h6>"); // h6
+  };
+  
+  
+  if (typeof module !== 'undefined' && typeof exports === 'object') {
+    module.exports = marka;
+  } else {
+      window.marka = marka;
+  }
+})();
